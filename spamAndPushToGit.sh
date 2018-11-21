2c53a173c6e2ff5e25ca3aa825bff915c049cea7
Azadi Bogolubov <azadibogolubov@gmail.com>
2018-11-20 16:28:04 -0800

    spamming atm at 2018-11-20T16:28:04

--- spamAndPushToGit.sh
+++ spamAndPushToGit.sh
@@ -4,7 +4,7 @@ cd ~/Desktop/githubSpammer;
 date=`date '+%Y-%m-%dT%H:%M:%S'`;
 sudo touch $date.txt;
 git add .;
-git commit -m "spamming atm";
+git commit -m "spamming atm at $date";
 git push origin master;
 
 
